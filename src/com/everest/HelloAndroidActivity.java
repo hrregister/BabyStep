1eebcaca9683012d05bc3117af2b5256e03f4cbf
Rong Huang <hrregisterthu@gmail.com>
2012-01-04 14:39:17 +0800

    first commit

---
+++ src/com/everest/HelloAndroidActivity.java
@@ -0,0 +1,17 @@
+package com.everest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class HelloAndroidActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        TextView tv = new TextView(this);
+//        tv.setText("hello world");
+//        setContentView(tv);
+        setContentView(R.layout.main);
+    }
+}
